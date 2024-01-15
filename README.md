703f331089620eeafa402d1fad8481c6923cc78a
spritchard54 <>
2024-01-15 12:36:41 -0500

    Fixed menu, general cleanup, removed separators, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# SQL_employeeTracker
-This application is a command-line application built from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+# SQL_employeeTracker ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+![Readme.md Screenshot](./Assets/Images/Screenshot%202024-01-15%20at%2010.19.20 AM.png)
+
+## Table of Contents
+[Description](#description)<br>
+[Installation](#installation)<br>
+[Usage](#usage)<br>
+[Contributing](#contributing)<br>
+[Tests](#tests)<br>
+[License Link](#license-link)<br>
+[License Description](#license-description)<br>
+[Questions](#questions)
+
+## Description
+This is a command line interface (CLI) application built from scratch that is used to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+## Installation
+After downloading the files to your local server, open the index.js file in a new integrated terminal. Install all required packages and then run "node index" from terminal.
+
+## Usage 
+1. Use your keyboard arrow keys to navigate up and down in the menu.
+2. Click enter to select the action you wish to take.
+3. Follow the prompts, enter the required response and cick enter on your keyboard.
+4. Confirm in the database that your changes have been made if you wish. 
+    
+## Contributing 
+EDX bootcamp instructors.
+    
+## Tests
+No tests were run for this application.
+
+## License Link
+https://www.mit.edu/~amini/LICENSE.md
+
+## License Description
+MIT License
+
+Copyright (c) 2023 spritchard54
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    
+## Questions
+- Github: https://github.com/spritchard54
+- Email: <swpritchard54@gmail.com>
--- index.js
+++ index.js
@@ -5,46 +5,23 @@ const express = require('express')
 const inquirer = require('inquirer')
 const app = express();
 const createConnection = require('./db/envConnect')
-const questions = [
-  {
-    type: 'list',
-    name: 'questionOne',
-    message: 'What do you want to do.',
-    choices: [
-      ' View all departments',
-      ' View all roles',
-      ' View all employees',
-      new inquirer.Separator(),
-      ' Add a department',
-      ' Add a role',
-      ' Add an employee',
-      new inquirer.Separator(),
-      ' Update an employee role',
-      new inquirer.Separator(),]
-    , default: ''
-  }]
 let db = null;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// menu function
+// Menu
 async function menu() {
   menuChoices = [
     "View all departments",
     "View all roles",
     "View all employees",
+    // new inquirer.Separator(),
     "Add a department",
     "Add a role",
     "Add an employee",
+    // new inquirer.Separator(),
     "Update an employee's role",
-    "Update an employee's manager",
-    "View employees by manager",
-    "View employees by department",
-    "Delete a department",
-    "Delete a role",
-    "Delete an employee",
-    "View the total utilized budget of a department",
-    "Exit",
+    "Quit",
   ];
   const answers = await inquirer.prompt([
     {
@@ -57,7 +34,6 @@ async function menu() {
   ]);
 
   //The switch statement evaluates an expression, matching the expression's value against a series of case clauses, and executes statements after the first case clause with a matching value, until a break statement is encountered. The default clause of a switch statement will be jumped to if no case matches the expression's value.
-
   switch (answers.option) {
     // If the user seclect "View all Departments", the run the viewDepartments function.
     case menuChoices[0]:
@@ -75,41 +51,28 @@ async function menu() {
     case menuChoices[6]:
       return updateEmployeeRole();
     case menuChoices[7]:
-      return updateEmployeeManager();
-    case menuChoices[8]:
-      return viewEmployeeByManager();
-    case menuChoices[9]:
-      return viewEmployeeByDepartment();
-    case menuChoices[10]:
-      return deleteDepartment();
-    case menuChoices[11]:
-      return deleteRole();
-    case menuChoices[12]:
-      return deleteEmployee();
-    case menuChoices[13]:
-      return viewTotalBudget();
-    case menuChoices[14]:
-      return exit();
+      return quit();
   }
 }
-
-// function to view all Departments
+// View all Departments
 async function viewDepartments() {
   const [depData, value] = await db.query(
     "SELECT id AS department_id, name AS department_name FROM department"
   );
+  console.table(depData);
   // brings us back to the menu
   return await menu();
 }
-// function to view all Departments
+// View all Roles
 async function viewRoles() {
   const [roleData, value] =
     await db.query(`SELECT role.id, title, salary, department.name AS deparment_name
   FROM role 
   LEFT JOIN department ON role.department_id = department.id`);
+  console.table(roleData);
   return await menu();
 }
-// function to view all Departments
+// View all Employees
 async function viewEmployees() {
   const [empData, value] = await db.query(
     `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager 
@@ -118,9 +81,10 @@ async function viewEmployees() {
     LEFT JOIN department ON role.department_id = department.id 
     LEFT JOIN employee AS manager ON employee.manager_id = manager.id`
   );
+  console.table(empData);
   return await menu();
 }
-// function to add a department
+// Add a department
 async function addDepartment() {
   // stores user answer from the prompt
   const answers = await inquirer.prompt([
@@ -128,7 +92,7 @@ async function addDepartment() {
       type: "input",
       // name matches the column you're trying to populate or fill
       name: "name",
-      message: "Enter the name of the department do you want to add",
+      message: "Enter the name of the new department.",
     },
   ]);
   // sql query with error handling
@@ -140,17 +104,11 @@ async function addDepartment() {
   );
   return await menu();
 }
-// function to add a role
+// Add a role
 async function addRole() {
-  // departmentData is the list of rows / objects
-  // metaData contains data types for the columns
-  // destructuring simplifies the return code
-  const [departmentData, metaData] = await db.query(
-    "SELECT id, name FROM department"
-  );
-  // creating a key-value pair for id and name
-  // this array is also used as the list of multiple choices prompted to the user in the list type questions
-  const departmentChoices = departmentData.map((row) => ({
+  const [depData, value] = await db.query("SELECT id, name FROM department");
+  // key-value pair
+  const departmentChoices = depData.map((row) => ({
     name: row.name,
     value: row.id,
   }));
@@ -182,12 +140,9 @@ async function addRole() {
       console.error(err);
     }
   );
-  console.log(inputData);
-
-  console.log("Insert Sucessful\n\n\n\n");
   return await menu();
 }
-// function to add a employee
+// Add a employee
 async function addEmployee() {
   const [roleData, metaData] = await db.query("SELECT title, id FROM role");
   // creates a key-value pair between title and id from the role's table
@@ -200,7 +155,7 @@ async function addEmployee() {
   );
   // creating a no manger option with a value of null to add to the array of choices
   const nullValue = { name: "None", value: null };
-  // concatenating the first_name and last_name values
+  // concate first_name and last_name values
   // creating a key-value pair between manager's name and id from the employee table
   const managerChoices = managerData.map((row) => ({
     name: row.first_name + " " + row.last_name,
@@ -231,16 +186,16 @@ async function addEmployee() {
       choices: managerChoices,
     },
   ]);
-
   const inputData = await db.query(
     "INSERT INTO employee SET ?", answers,(err, results) => {
       if (err) throw err;
       console.error(err);
     }
   );
+  console.log(inputData);
   return await menu();
 }
-// function to update an employee's role
+// Update an employee's role
 async function updateEmployeeRole() {
   const [empData, employeeMetaData] = await db.query(
     "SELECT first_name, last_name, id FROM employee"
@@ -279,187 +234,11 @@ async function updateEmployeeRole() {
   });
   return await menu();
 }
-// function to update an employee's manager
-async function updateEmployeeManager() {
-  const [empData, employeeMetaData] = await db.query(
-    "SELECT first_name, last_name, id, manager_id FROM employee"
-  );
-  // creating an array of objects with key-value pairs for name, id, and manager id
-  const employeeChoices = empData.map((row) => ({
-    name: row.first_name + " " + row.last_name,
-    value: row.id,
-    manager: row.manager_id,
-  }));
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "id",
-      message: "Which employee do you want to update?",
-      choices: employeeChoices,
-    },
-    {
-      type: "list",
-      name: "manager_id",
-      message: "Which manager do you want to assign the employee?",
-      choices: employeeChoices,
-    },
-  ]);
-  const sql = "UPDATE employee SET manager_id = ? WHERE id = ? ";
-  const sqlParams = [answers.manager_id, answers.id];
-  const inputData = await db.query(sql, sqlParams, (err, results) => {
-    if (err) throw err;
-    console.error(err);
-  });
-  return await menu();
-}
-// views employees by manager
-async function viewEmployeeByManager() {
-  console.log("Viewing Employees by Manager");
-  const [empData, employeeMetaData] = await db.query(
-    `SELECT CONCAT(manager.first_name, " ", manager.last_name) AS manager, employee.id, employee.first_name, employee.last_name 
-    FROM employee 
-    LEFT JOIN role ON employee.role_id = role.id 
-    LEFT JOIN department ON role.department_id = department.id 
-    LEFT JOIN employee AS manager ON employee.manager_id = manager.id
-    WHERE employee.manager_id IS NOT NULL
-    ORDER BY manager`
-  );
-  console.table(empData);
-  return await menu();
-}
-// view employees by department
-async function viewEmployeeByDepartment() {
-  const [empData, employeeMetaData] = await db.query(
-    `SELECT department.name as department, employee.id, employee.first_name, employee.last_name
-    FROM employee 
-    LEFT JOIN role ON employee.role_id = role.id 
-    LEFT JOIN department ON role.department_id = department.id
-    ORDER BY department.name`
-  );
-  console.table(employeeData);
-  return await menu();
-}
-// function to delete a department
-async function deleteDepartment() {
-  console.log("Deleting Department");
-  const [depData, metaData] = await db.query("SELECT * FROM department");
-  const depChoices = depData.map((row) => ({
-    name: row.name,
-    value: row.id,
-  }));
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "id",
-      message: "What department do you want to delete?",
-      choices: depChoices,
-    },
-  ]);
-
-  const inputData = await db.query(
-    "DELETE FROM department WHERE id = ?",
-    answers.id,
-    (err, results) => {
-      if (err) throw err;
-      console.error(err);
-    }
-  );
-
-  return await menu();
-}
-// function to delete a role
-async function deleteRole() {
-  const [roleData, metaData] = await db.query("SELECT title, id FROM role");
-  const roleChoices = roleData.map((row) => ({
-    name: row.title,
-    value: row.id,
-  }));
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "id",
-      message: "What role do you want to delete?",
-      choices: roleChoices,
-    },
-  ]);
-
-  const inputData = await db.query(
-    "DELETE FROM role WHERE id = ?",
-    answers.id,
-    (err, results) => {
-      if (err) throw err;
-      console.error(err);
-    }
-  );
-  console.log(inputData);
-
-  console.log("Delete Sucessful\n\n\n\n");
-  return await menu();
-}
-// function to delete an employee
-async function deleteEmployee() {
-  console.log("Deleting Employeee");
-  const [empData, metaData] = await db.query(
-    "SELECT first_name, last_name, id FROM employee"
-  );
-  const empChoices = empData.map((row) => ({
-    name: row.first_name + " " + row.last_name,
-    value: row.id,
-  }));
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "id",
-      message: "What role do you want to delete?",
-      choices: empChoices,
-    },
-  ]);
-
-  const inputData = await db.query(
-    "DELETE FROM employee WHERE id = ?",
-    answers.id,
-    (err, results) => {
-      if (err) throw err;
-      console.error(err);
-    }
-  );
-  console.log(inputData);
-  return await menu();
-}
-// view employees by department
-async function viewTotalBudget() {
-  console.log("Viewing Total Budget");
-  const [depData, metaData] = await db.query("SELECT * FROM department");
-  const depChoices = depData.map((row) => ({
-    name: row.name,
-    value: row.id,
-  }));
-
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "id",
-      message: "What department do you want to view the budget for?",
-      choices: depChoices,
-    },
-  ]);
-
-  const [totalBudgetData, metaBudgetData] = await db.query(
-    `SELECT department.name as department, SUM(role.salary) AS total_budget
-    FROM employee 
-    LEFT JOIN role ON employee.role_id = role.id 
-    LEFT JOIN department ON role.department_id = department.id 
-    WHERE department.id = ?`,
-    answers.id
-  );
-
-  console.table(totalBudgetData);
-  return await menu();
-}
-// exit function
-async function exit() {
+// Quit function
+async function quit() {
   process.exit();
 }
+// Run the Program
 async function init() {
   // connects to the database/ use await to wait for connection to finish
   db = await createConnection();
